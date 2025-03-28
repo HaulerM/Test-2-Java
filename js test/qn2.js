00912e46bc9d2be613bc76c0b468adb3d141480d
HaulerM <haulermusa18@gmail.com>
2025-03-28 02:58:16 -0700

    Mid Exam

--- js test/qn2.js
+++
@@ -1,3 +0,0 @@
-// Objects are collections of key-value pairs.
-//  while
-//Arrays are special variable which can hold more than one value.
---
+++ sectionb.js
@@ -0,0 +1,49 @@
+//1
+// var has function scope and can be redeclared.
+
+//const is also block-scoped but must be initialized and cannot be reassigned.
+
+//let has block scope and cannot be redeclared within the same scope.
+
+//2
+// JavaScript objects are collections of key-value pairs,
+//  while arrays are ordered collections indexed by numbers.
+//differnce.
+//  Objects are used for storing structured data, whereas arrays are best for lists.
+
+//3
+//The "==' check for value equality and performs type coercion,
+// while the '===' checks for both value and type without type conversion.
+
+//4
+// An event is an action that happens in the browser.
+
+// Example:
+
+
+document.getElementById("myButton").addEventListener("click", function() {
+
+    alert("Button clicked!");
+
+});
+
+//5
+//The forEach method excutes a provided function once for each array element.
+
+// The map method applies a function to each element of an array and returns a new array with transformed values.
+
+//similarity
+
+//They both accept a callback function that is excuted on each element.
+
+//Difference.
+
+//The forEach method doesn't return a new array while the map method returns a new array.
+
+
+
+
+
+
+
+
